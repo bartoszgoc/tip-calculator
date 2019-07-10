22cd16a785b49b533b2986a052463f5bd94f60ac
Bartosz Goc <bartoszgoc@gmail.com>
2019-07-10 23:08:56 +0200

    feat: add buttons and input the logic

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+const buttonCalculate = document.querySelector('.button__calculate--js');
+const buttonClear = document.querySelector('.button__clear--js');
+
+buttonCalculate.addEventListener('click', (e) => {
+    const totalBill = document.querySelector('.item-bill--js').value
+    const tipPercent = document.querySelector('.item-percent--js').value
+    const pplNumber = document.querySelector('.item-ppl--js').value
+
+
+    const result = Math.floor((+totalBill + +tipPercent)/+pplNumber);
+    console.log(`${result}`)
+})
